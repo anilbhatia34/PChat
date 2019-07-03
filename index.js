333241d50ad6bb25b90b3ef0eb1399baf523e3e9
anil <anilbhatia34@gmail.com>
2019-07-03 22:26:50 +0530

    TEST

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express=require('express');
-
+const socket = require('socket.io');
 
 //app setup
 
 const app=express();
 const server=app.listen(4000, ()=>{
     console.log("app is lisnten on 4000 port");
+});
+
+//static file
+
+app.use(express.static('public'));
+
+// Socket setup & pass server
+const io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+   // Handle chat event
+   socket.on('chat', function(data){
+    // console.log(data);
+    io.sockets.emit('chat', data);
+});
+
+// Handle typing event
+socket.on('typing', function(data){
+    socket.broadcast.emit('typing', data);
+});
+
+
 });
