18885726d6bc858315ce5d147f611fa3fd43655d
anil <anilbhatia34@gmail.com>
2019-07-03 23:14:09 +0530

    changes
    fea

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const socket = require('socket.io');
-
+const port = process.env.PORT || 4000
 //app setup
 
 const app=express();
-const server=app.listen(4000, ()=>{
+const server=app.listen(port, ()=>{
     console.log("app is lisnten on 4000 port");
 });
 
