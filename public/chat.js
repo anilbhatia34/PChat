f7ae8b8162cd4930c7394eb711b321950315d0ba
anil <anilbhatia34@gmail.com>
2019-07-04 00:13:28 +0530

    changes
    a

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-const socket = io.connect('http://localhost:4000');
+const socket = io.connect('https://intelligaiachatsystem.herokuapp.com/');
 
 // Query DOM
 var message = document.getElementById('message'),
