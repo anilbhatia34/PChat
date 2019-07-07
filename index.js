bde8884c29292f6173bc3575d528a80a59aa30b3
anil <anilbhatia34@gmail.com>
2019-07-07 23:13:43 +0530

    CHANGES
    ERERE

--- index.js
+++ index.js
@@ -9,25 +9,18 @@ const server=app.listen(port, ()=>{
 });
 
 //static file
-
 app.use(express.static('public'));
-
 // Socket setup & pass server
 const io = socket(server);
 io.on('connection', (socket) => {
-
     console.log('made socket connection', socket.id);
-
    // Handle chat event
    socket.on('chat', function(data){
     // console.log(data);
     io.sockets.emit('chat', data);
 });
-
 // Handle typing event
 socket.on('typing', function(data){
     socket.broadcast.emit('typing', data);
 });
-
-
 });
